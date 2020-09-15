7714b8f79c3359c0dadedabb869e9b87ef6ae22e
Abdullah0095 <abdullah.web1995@gmail.com>
2020-09-16 01:42:29 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>amake ebar partei hobe, etay moja ase
+        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
